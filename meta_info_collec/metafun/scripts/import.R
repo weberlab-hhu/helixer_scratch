a67d1b2f39d47cb571001fb9a32c8ffb61b53978
alisandra <alisandra.denton@hhu.de>
2019-10-04 17:11:11 +0200

    look @ meta-predictors for cds & aug cds

--- meta_info_collec/metafun/scripts/import.R
+++ meta_info_collec/metafun/scripts/import.R
@@ -1,4 +1,5 @@
 metadata <- read.csv('metadata.csv')
 predacc9 <- read.csv('single_genome_results_10k_LSTM_O647Aaic_genomes.csv')
 predacc4 <- read.csv('single_genome_results_5k.csv')
-
+acc9withcds <- read.csv('single_genome_results_10k_LSTM_O647Aaic_CDS.csv')
+predacc9 <- merge(predacc9, acc9withcds[c("genome", "f1_cds_1")], by="genome")
