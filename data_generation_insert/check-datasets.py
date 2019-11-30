e2621fd0b4f1709362a090429b0b99c480b340b4
Felix Stiehler <felix.stiehler87@gmail.com>
2019-11-30 18:28:24 +0100

    script to check many generated datasets for completion

---
+++ data_generation_insert/check-datasets.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+'''Checks if all .h5 files in the given folder have attributes, which means they
+should have all the data'''
+
+import glob
+import os
+import h5py
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--folder', type=str, required=True)
+parser.add_argument('-sf', '--sub-folder', type=str, default='')
+args = parser.parse_args()
+
+for genome_folder in os.listdir(args.folder):
+    if args.sub_folder == '':
+        h5_files = glob.glob(os.path.join(args.folder, genome_folder, '*.h5'))
+    else:
+        h5_files = glob.glob(os.path.join(args.folder, genome_folder, args.sub_folder, '*.h5'))
+
+    for h5_file in h5_files:
+        f = h5py.File(h5_file, 'r')
+        if 'genomes' in f.attrs:
+            print('OK', h5_file)
+        else:
+            print('ERROR', h5_file)
