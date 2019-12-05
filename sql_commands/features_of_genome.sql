9eb66e2ccb4e0eb4b3c3b1b4bbced80b0963ca8b
Felix Stiehler <felix.stiehler87@gmail.com>
2019-12-05 09:59:03 +0100

    sql

--- sql_commands/features_of_genome.sql
+++ sql_commands/features_of_genome.sql
@@ -148,3 +148,26 @@ SELECT species, sum(intron_sum), sum(length), sum(intron_sum) * 1.0 / sum(length
 )
 GROUP BY species;
 
+-- gene extends plus strand
+SELECT coordinate.seqid, min(feature.start), max(feature.end), count(distinct(transcript.id)) FROM genome
+CROSS JOIN coordinate ON coordinate.genome_id = genome.id
+CROSS JOIN feature ON feature.coordinate_id = coordinate.id
+CROSS JOIN association_transcript_piece_to_feature ON association_transcript_piece_to_feature.feature_id = feature.id
+CROSS JOIN transcript_piece ON association_transcript_piece_to_feature.transcript_piece_id = transcript_piece.id
+CROSS JOIN transcript ON transcript_piece.transcript_id = transcript.id
+CROSS JOIN super_locus ON transcript.super_locus_id = super_locus.id
+WHERE genome.species IN ('Athaliana') AND super_locus.type = 'gene' AND transcript.type = 'mRNA'
+	AND feature.type = 'geenuff_transcript' AND feature.is_plus_strand = 1
+GROUP BY super_locus.id;
+
+-- gene extends minus strand (with flipped start/end)
+SELECT coordinate.seqid, min(feature.end) + 1, max(feature.start) + 1, count(distinct(transcript.id)) FROM genome
+CROSS JOIN coordinate ON coordinate.genome_id = genome.id
+CROSS JOIN feature ON feature.coordinate_id = coordinate.id
+CROSS JOIN association_transcript_piece_to_feature ON association_transcript_piece_to_feature.feature_id = feature.id
+CROSS JOIN transcript_piece ON association_transcript_piece_to_feature.transcript_piece_id = transcript_piece.id
+CROSS JOIN transcript ON transcript_piece.transcript_id = transcript.id
+CROSS JOIN super_locus ON transcript.super_locus_id = super_locus.id
+WHERE genome.species IN ('Athaliana') AND super_locus.type = 'gene' AND transcript.type = 'mRNA'
+	AND feature.type = 'geenuff_transcript' AND feature.is_plus_strand = 0
+GROUP BY super_locus.id;
