1052c8709670449aa115d04984d1052aad98fd95
Felix Stiehler <felix.stiehler87@gmail.com>
2019-12-11 10:55:17 +0100

    script

--- data_generation_insert/length-wise-eval-from-nni.sh
+++ data_generation_insert/length-wise-eval-from-nni.sh
@@ -2,10 +2,8 @@
 
 trial_id=$1
 trial_path="/home/felix-stiehler/nni/experiments/QHKf7JfJ/trials/$1/"
+data_path="/home/felix-stiehler/Desktop/data/plants/single_genomes_new/"
 
 genome=$(cat "$trial_path"parameter.cfg | cut -d "{" -f3 | cut -d "/" -f8)
 
-echo $genome
-
-
-
+/home/felix-stiehler/git/HelixerPrep/scripts/errors_position_wise.py -p "$trial_path"predictions.h5 -d "$data_path$genome"/test_data.h5 -g $genome &> eval_"$genome"
