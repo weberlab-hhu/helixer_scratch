96e733d81cc2a833b3f6b19b0f18b97f7be4d072
Felix Stiehler <felix.stiehler87@gmail.com>
2019-12-13 11:05:25 +0100

    gene stats

---
+++ data_insight/gene_stats.py
@@ -0,0 +1,8 @@
+def count(f, training_genomes):
+    values = []
+    for i, line in enumerate(open(f)):
+        if i > 1 and (not training_genomes or line.strip().split()[0] in training_genomes):
+            values.append(int(line.strip().split()[2]))
+    print('median', np.median(values))
+    print('mean', np.mean(values))
+    print('std', np.std(values))
