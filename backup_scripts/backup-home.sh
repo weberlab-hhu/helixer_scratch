393cc124e8fa2613974e9c277e0debf8424b5278
Felix Stiehler <felix.stiehler87@gmail.com>
2019-12-21 16:26:02 +0100

    backup script

--- backup_scripts/backup-home.sh
+++ backup_scripts/backup-home.sh
@@ -9,6 +9,7 @@ then
 	rsync -rzt --stats -e 'ssh -A -o "ProxyJump felix@134.99.224.62"' /home/felix/nni felix@134.99.224.58:/mnt/data/experiments_backup/nni_home_felix/
 	rsync -rzt --stats -e 'ssh -A -o "ProxyJump felix@134.99.224.62"' /home/felix/.local/nnictl felix@134.99.224.58:/mnt/data/experiments_backup/nni_home_felix/
 else
-	echo "Not connected to the vpn"
+	echo "Not connected to the vpn, writing data to the cluster"
+	rsync -rzt --stats --exclude='*model*.h5' /home/felix/nni festi100@hpc.rz.uni-duesseldorf.de:/gpfs/project/festi100/backup_home/
+	rsync -rzt --stats /home/felix/.local/nnictl festi100@hpc.rz.uni-duesseldorf.de:/gpfs/project/festi100/backup_home/
 fi
-
