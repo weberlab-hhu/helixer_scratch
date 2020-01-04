848dc5b6f9dd69db12fd93b3ef703981fe601f8c
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-04 10:12:25 +0100

    start_many.sh

---
+++ cluster_eval/start_many.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+if [[ $# -lt 1 ]]; then
+	echo "Usage: ./start_many.sh n_qsubs"
+	exit
+fi
+
+if [[ ! -f "next_line" ]]; then
+	echo "next_line file not found. Script has to be run from the root dir."
+	exit
+fi
+
+line_offset=$(<next_line)
+n_lines=$(cat datasets | wc -l)
+if [[ $(($line_offset+$1)) -gt $n_lines ]]; then
+	n_qsubs=$(($n_lines-$line_offset))
+else
+	n_qsubs=$1
+fi
+
+for i in $(seq 1 $n_qsubs); do
+	./start_eval.sh /gpfs/project/festi100/models/ZCPHo_6.h5 /gpfs/project/festi100/data/animals/
+done
