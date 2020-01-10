6cce3798b45d088020bd35809fbecafeba77f955
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-10 09:11:57 +0100

    prediction eval fixed for cluster

--- data_scripts/eval-from-predictions-one-trial.sh
+++ data_scripts/eval-from-predictions-one-trial.sh
@@ -11,11 +11,10 @@ output_file_name=$2
 # if the folder has a trial.log in it we assume nni, otherwise cluster
 if [[ -f "$trial_folder/trial.log" ]]; then
 	log_file="$trial_folder/trial.log"
-	dataset=$(grep "test_data.h5" "$log_file"| cut -d " " -f5 | tr -d "'")
+	dataset=$(grep "test_data.h5" $log_file| cut -d " " -f5 | tr -d "'")
 else
 	log_file="$trial_folder/*.sh.o*"
-	dataset=$(grep "test_data.h5" "$log_file"| cut -d " " -f3 | tr -d ",'")
+	dataset=$(grep "test_data.h5" $log_file| cut -d " " -f3 | tr -d ",'")
 fi
 
-/home/felix-stiehler/git/HelixerPrep/scripts/basic_pred_cm.py -d $dataset -p "$trial_folder/predictions.h5" &>> "$trial_folder/$output_file_name"
-
+~/git/HelixerPrep/scripts/basic_pred_cm.py -d $dataset -p "$trial_folder/predictions.h5" &>> "$trial_folder/$output_file_name"
