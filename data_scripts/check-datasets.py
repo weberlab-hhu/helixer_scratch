d547e2a30b9344b212d9a8a984612bd8ea3e7d4f
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-16 15:41:14 +0100

    check-datasets.py can not delete folders with errors

--- data_scripts/check-datasets.py
+++ data_scripts/check-datasets.py
@@ -7,27 +7,39 @@ import glob
 import os
 import h5py
 import argparse
+import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument('folder', type=str)
 parser.add_argument('-sf', '--sub-folder', type=str, default='')
+parser.add_argument('-del', '--delete', action='store_true', help='Remove folders with errors')
 args = parser.parse_args()
 
 for genome_folder in os.listdir(args.folder):
-    if args.sub_folder == '':
-        h5_files = glob.glob(os.path.join(args.folder, genome_folder, '*.h5'))
-    else:
-        h5_files = glob.glob(os.path.join(args.folder, genome_folder, args.sub_folder, '*.h5'))
-
-    for h5_file in h5_files:
-        try:
-            f = h5py.File(h5_file, 'r')
-        except OSError:
-            print('ERROR', h5_file)
-            continue
-
-        if 'timestamp' in f.attrs:
-            # the attributes should be written after all data has been saved
-            print('OK', h5_file)
+    folder = os.path.join(args.folder, genome_folder)
+    if os.path.isdir(folder):
+        if args.sub_folder == '':
+            h5_files = glob.glob(os.path.join(folder, '*.h5'))
         else:
-            print('ERROR', h5_file)
+            h5_files = glob.glob(os.path.join(folder, args.sub_folder, '*.h5'))
+
+        # switch whether to delete the whole folder if --delete is set
+        # also deletes if dir does not contain h5 files
+        found_only_errors = True
+        for h5_file in h5_files:
+            try:
+                f = h5py.File(h5_file, 'r')
+            except OSError:
+                print('ERROR', h5_file)
+                continue
+
+            if 'timestamp' in f.attrs:
+                # the attributes should be written after all data has been saved
+                print('OK', h5_file)
+                found_only_errors = False # one good file stops the folder purge
+            else:
+                print('ERROR', h5_file)
+
+        if args.delete and found_only_errors:
+            shutil.rmtree(folder)
+            print(folder, 'deleted')
