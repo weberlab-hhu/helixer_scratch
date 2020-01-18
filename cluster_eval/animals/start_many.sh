6098c1cca18662ce1aa3aa35f2f0ae989ed8f753
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-18 14:14:08 +0100

    cluster eval plants

---
+++ cluster_eval/animals/start_many.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+if [[ $# -lt 3 ]]; then
+	echo "Usage: ./start_many.sh model_file main_data_folder n_qsubs"
+	exit
+fi
+
+if [[ ! -f "next_line" ]]; then
+	echo "next_line file not found. Script has to be run from the root dir."
+	exit
+fi
+
+if [[ $(basename $(pwd)) = "cluster_eval" ]]; then
+	echo "script seems to be run from the repository, which should be an error"
+	exit
+fi
+
+model_file=$1
+main_data_folder=$2
+
+line_offset=$(<next_line)
+n_lines=$(cat datasets | wc -l)
+if [[ $(($line_offset+$3)) -gt $(($n_lines+1)) ]]; then
+	n_qsubs=$(($n_lines-$line_offset+1))
+else
+	n_qsubs=$3
+fi
+
+for i in $(seq 1 $n_qsubs); do
+	./start_eval.sh $model_file $main_data_folder
+done
---
+++ cluster_eval/plants/mk-predictions-plants.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+# generates overlapping single genome plant predictions for a given model
+# for the plants, the input length does not vary
+
+if [[ $# -lt 2 ]]; then
+	echo "Usage: ./mk-predictions-plants.sh job_folder model_file"
+	exit
+fi
+
+job_folder=$1
+model_file=$2
+data_main_folder="/gpfs/project/festi100/data/plants/single_genomes"
+
+for data_folder in $(ls -d "$data_main_folder"/*/); do
+	species=$(basename $data_folder)
+	job_subfolder=$job_folder/$species
+	if [[ ! -d $job_subfolder ]]; then
+		mkdir -p $job_subfolder
+		qsub_file_path=$job_subfolder/$species".sh"
+		cat <<- EOF > $qsub_file_path
+		#!/bin/bash
+		#PBS -l select=1:ncpus=1:mem=3:ngpus=1:accelerator_model=gtx1080ti
+		#PBS -l walltime=7:59:00
+		#PBS -A "HelixerOpt"
+
+		##Call, also run again if an error occurs (can happen seemingly at random)
+		/home/festi100/git/HelixerPrep/helixerprep/prediction/LSTMModel.py -v -bs 400 -lm $model_file -td $data_main_folder/$species/test_data.h5 -po \$PBS_O_WORKDIR/predictions.h5 --overlap --overlap-offset 2500 --core-length 10000 || /home/festi100/git/HelixerPrep/helixerprep/prediction/LSTMModel.py -v -bs 400 -lm $model_file -td $data_main_folder/$species/test_data.h5 -po \$PBS_O_WORKDIR/predictions.h5 --overlap --overlap-offset 2500 --core-length 10000
+		EOF
+
+		echo -n $species" "
+		cd $job_subfolder
+		qsub $species".sh"
+	else
+		echo "Folder $job_subfolder exists. skipping."
+	fi
+
+	sleep 1 # to check everything
+done
