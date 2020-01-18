ec4ccb74094dfde34687d465b3a0bf55932efc85
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-18 18:34:09 +0100

    minor fix

--- cluster_eval/plants/mk-predictions-plants.sh
+++ cluster_eval/plants/mk-predictions-plants.sh
@@ -20,9 +20,11 @@ for data_folder in $(ls -d "$data_main_folder"/*/); do
 		qsub_file_path=$job_subfolder/$species".sh"
 		cat <<- EOF > $qsub_file_path
 		#!/bin/bash
-		#PBS -l select=1:ncpus=1:mem=3:ngpus=1:accelerator_model=gtx1080ti
+		#PBS -l select=1:ncpus=1:mem=3gb:ngpus=1:accelerator_model=gtx1080ti
 		#PBS -l walltime=7:59:00
 		#PBS -A "HelixerOpt"
+
+		module load TensorFlow/1.10.0
 
 		##Call, also run again if an error occurs (can happen seemingly at random)
 		/home/festi100/git/HelixerPrep/helixerprep/prediction/LSTMModel.py -v -bs 400 -lm $model_file -td $data_main_folder/$species/test_data.h5 -po \$PBS_O_WORKDIR/predictions.h5 --overlap --overlap-offset 2500 --core-length 10000 || /home/festi100/git/HelixerPrep/helixerprep/prediction/LSTMModel.py -v -bs 400 -lm $model_file -td $data_main_folder/$species/test_data.h5 -po \$PBS_O_WORKDIR/predictions.h5 --overlap --overlap-offset 2500 --core-length 10000
