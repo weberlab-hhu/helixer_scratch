dc3b932a5ff250883b1a344a42d94253ca6f945d
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-22 11:11:09 +0100

    stuff

--- data_scripts/insert-full.sh
+++ data_scripts/insert-full.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-db_path='/mnt/data/felix/animal_geenuff.sqlite3'
+# db_path='/mnt/data/felix/animal_geenuff.sqlite3'
+db_path='/home/felix/Desktop/animal_geenuff_test.sqlite3'
 
-rm -v $db_path;
-ls -1 /mnt/data/ali/share/ensembl_nosplit/ | xargs -I % ~/Desktop/data_scripts/insert-one.sh % $db_path
+ls -1 /mnt/data/ali/share/ensembl_test | xargs -I % /home/felix/git/helixer_scratch/data_scripts/insert-one.sh % $db_path
 
-# ge; ls -1 /mnt/data/ali/share/phytozome_organized/ready/train | xargs -I % ./import_genome.py --db-path ~/Desktop/geenuff_plants.sqlite3 --basedir /mnt/data/felix/rawdata/plants/%/ --species %
+# ge; ls -1 /mnt/data/ali/share/phytozome_organized/ready/test | xargs -I % ./import_genome.py --db-path ~/Desktop/geenuff_plants.sqlite3 --basedir /mnt/data/felix/rawdata/plants/%/ --species %
--- data_scripts/insert-one.sh
+++ data_scripts/insert-one.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
 geenuff_basedir=~/git/GeenuFF
-data_basedir=/mnt/data/ali/share/ensembl_nosplit
+data_basedir=/mnt/data/ali/share/ensembl_test
+# data_basedir=/mnt/data/ali/share/ensembl_nosplit
+# data_basedir=/mnt/data/ali/share/phytozome_organized/ready/test
 genome=$1
 db_path=$2
 
 rm -v -r $geenuff_basedir/tmp
 mkdir -v -p $geenuff_basedir/tmp/input $geenuff_basedir/tmp/output
-gunzip -v -c $data_basedir/$genome/ensembl98/annotation/*.gff3.gz > $geenuff_basedir/tmp/input/$genome.gff3
-gunzip -v -c $data_basedir/$genome/ensembl98/assembly/*.fa.gz > $geenuff_basedir/tmp/input/$genome.fa
+
+# gunzip -v -c $data_basedir/$genome/ensembl98/annotation/*.gff3.gz > $geenuff_basedir/tmp/input/$genome.gff3
+# gunzip -v -c $data_basedir/$genome/ensembl98/assembly/*.fa.gz > $geenuff_basedir/tmp/input/$genome.fa
+
+cp -v $data_basedir/$genome/ensembl98/annotation/*.gff3 $data_basedir/$genome/ensembl98/assembly/*.fa $geenuff_basedir/tmp/input/
 
 $geenuff_basedir/import_genome.py --db-path $2 --basedir $geenuff_basedir/tmp/ --species $genome
