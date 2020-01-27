9a9d04050defe0ad48cd91ab160796d7956fc053
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-27 10:52:46 +0100

    minor

--- cluster_eval_predictions/predictions/ensembling/ensemble_/gpfs/project/festi100/jobs/IiFEB_IiFEB_11/Acomosus/Acomosus_ensemble.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#PBS -l select=1:ncpus=1:mem=200mb
-#PBS -l walltime=7:59:00
-#PBS -A "HelixerOpt"
-
-module load Python/3.6.5
-
-/home/festi100/git/HelixerPrep/scripts/ensemble.py  -p /gpfs/project/festi100/jobs//IiFEB_11/Acomosus/predictions.h5 -p /gpfs/project/festi100/jobs//IiFEB/Acomosus/predictions.h5 -po ensemble_/gpfs/project/festi100/jobs//IiFEB_IiFEB_11/Acomosus/predictions.h5
