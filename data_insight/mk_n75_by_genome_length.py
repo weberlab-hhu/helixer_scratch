250761e856a93c2806ea6e7ab68d3acaba59a1ba
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-29 14:20:06 +0100

    n75 by genome length stuff

--- data_insight/mk_n75_by_genome_length.py
+++ data_insight/mk_n75_by_genome_length.py
@@ -8,10 +8,11 @@ import pandas as pd
 parser = argparse.ArgumentParser()
 parser.add_argument('--metadata_file', type=str, default='../metadata_csvs/plant_metadata.csv')
 parser.add_argument('--output_file', type=str, default='output.csv')
+parser.add_argument('--sort-by', type=str, default='N75/total_len')
 args = parser.parse_args()
 
 df = pd.read_csv(args.metadata_file)
 df['N75/total_len'] = df['N75'] / df['total_len']
-df = df.sort_values('N75/total_len', ascending=False)
+df = df.sort_values(args.sort_by, ascending=False)
 df.to_csv(args.output_file, columns=['species', 'N75', 'total_len', 'N75/total_len'], index=False)
 print(f'{args.output_file} written')
