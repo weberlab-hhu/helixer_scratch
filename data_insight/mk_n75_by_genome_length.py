802f5e71d858220c6994f49f37a69655d0c23ce9
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-29 13:32:41 +0100

    n75/genome_length csv output

---
+++ data_insight/mk_n75_by_genome_length.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+# reads in a metadata csv file and outputs a csv with just two columns: species,n75/genome_length
+
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--metadata_file', type=str, default='../metadata_csvs/plant_metadata.csv')
+parser.add_argument('--output_file', type=str, default='output.csv')
+args = parser.parse_args()
+
+df = pd.read_csv(args.metadata_file)
+df['N75/total_len'] = df['N75'] / df['total_len']
+df = df.sort_values('N75/total_len', ascending=False)
+df.to_csv(args.output_file, columns=['species', 'N75', 'total_len', 'N75/total_len'], index=False)
+print(f'{args.output_file} written')
