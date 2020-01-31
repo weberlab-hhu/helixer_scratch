9fca6d65ce1b5e2fe7fa210c9725905d91275a55
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-31 11:25:26 +0100

    minor

--- cluster_eval_predictions/predictions/queue_animals.sh
+++ cluster_eval_predictions/predictions/queue_animals.sh
@@ -2,7 +2,7 @@
 
 while true; do
 	for job in ZCPHo ZCPHo_6 duZo1 duZo1_7 lH8Hq lH8Hq_5 home home_5; do
-		echo "switching to $job"
+		echo -e "\nswitching to $job"
 
 		# remove all failed jobs and reset next_line count
 		job_folder=/gpfs/project/festi100/jobs/$job
@@ -18,7 +18,7 @@ while true; do
 		while true; do
 			# queue jobs if theoretically possible
 			# 30 jobs seems to be the maximum for now
-			if [[ $(qstat -u festi100 | grep CUDA | grep " R " | egrep -c -v "00:00$") -lt 30 ]]; then
+			if [[ $(qstat -u festi100 | grep CUDA | grep -c " R ") -lt 30 ]]; then
 				./start_eval.sh /gpfs/project/festi100/models/animals_final/$job".h5" /gpfs/project/festi100/data/animals 120
 				if [[ $? -eq 1 ]]; then
 					exit 1 # something went very wrong
