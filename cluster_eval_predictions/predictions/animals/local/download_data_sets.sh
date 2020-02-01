65037f7aeecc4070ec2bd8083dc5bb5535bd9231
Felix Stiehler <felix.stiehler87@gmail.com>
2020-02-01 20:07:23 +0100

    minor

---
+++ cluster_eval_predictions/predictions/animals/local/download_data_sets.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# download only the test_data.h5 files of the length per animal that is actually needed
+# for prediction according to the datasets file
+
+for line in $(<~/git/helixer_scratch/cluster_eval_predictions/predictions/animals/cluster/datasets); do
+
+
+done
