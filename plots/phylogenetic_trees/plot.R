2984353628cefddc3f3efc264818a2af787b55e9
Felix Stiehler <felix.stiehler87@gmail.com>
2020-04-03 11:24:17 +0200

    dataset classes, minor plotting stuff

--- plots/phylogenetic_trees/plot.R
+++ plots/phylogenetic_trees/plot.R
@@ -3,15 +3,15 @@ library(ggtree)
 library(treeio)
 library(tidytree)
 
-tree = read.nhx('~/git/helixer_scratch/plots/phylogenetic_trees/all_species.tre')
-tree_human = tree_subset(tree, node="Homosapiens", levels_back=5)
+tree = read.nhx('all_species.tre')
+# tree_human = tree_subset(tree, node="Homosapiens", levels_back=5)
 
 
-additional_data = read.csv('~/git/helixer_scratch/plots/phylogenetic_trees/additional_data.csv', header=TRUE)
-p = ggtree(tree_human) %<+% additional_data + geom_tiplab(size=2)
+f1_difference = read.csv('f1_difference.csv', header=TRUE)
+p = ggtree(tree) + geom_tiplab(size=4)
 
-png('/home/felix/git/helixer_scratch/plots/phylogenetic_trees/plot.png', width=800, height=2000)
-gheatmap(p, additional_data, high='darkgreen', low='red', colnames=F)
+png('animals.png', width=800, height=2000)
+gheatmap(p, f1_difference, width=.3, high='darkgreen', low='red', colnames=F, offset=9)
 dev.off()
 
 
