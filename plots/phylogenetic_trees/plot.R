3dc0d436f81a77bd30dc765b7b0b5bf833459246
Felix Stiehler <felix.stiehler87@gmail.com>
2020-04-03 14:33:53 +0200

    minor

--- plots/phylogenetic_trees/plot.R
+++ plots/phylogenetic_trees/plot.R
@@ -9,12 +9,10 @@ tree = read.nhx('all_species.tre')
 
 f1_difference = read.csv('f1_difference.csv', header=TRUE)
 sets = read.csv('sets.csv', header=TRUE)
-p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set))
+p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set)) 
+# p = p + geom_label(aes(label=node), fill='steelblue')
+p = p + geom_cladelabel(node=201, label="Mammalia", align=T, geom='label', offset=12)
 
-png('animals.png', width=800, height=2000)
-gheatmap(p, f1_difference, width=.3, high='darkgreen', low='red', colnames=F, offset=9)
+png('animals.png', width=1200, height=2000)
+gheatmap(p, f1_difference, width=.2, high='green', low='red', colnames=F, offset=5) 
 dev.off()
-
-
-
-
