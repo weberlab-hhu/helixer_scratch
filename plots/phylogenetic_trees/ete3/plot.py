033775077365f3b7cae201f3232009284a5483b4
Felix Stiehler <felix.stiehler87@gmail.com>
2020-04-06 18:38:45 +0200

    stuff

--- plots/phylogenetic_trees/ete3/plot.py
+++ plots/phylogenetic_trees/ete3/plot.py
@@ -5,15 +5,23 @@ from ete3 import ClusterTree, ProfileFace, ArrayTable, TreeStyle, AttrFace, Circ
 from ete3.treeview.faces import add_face_to_node
 
 # tree = ete3.PhyloTree("all_species.tre", sp_naming_function=lambda node: node.name)
-data = ArrayTable("results.array")
+data = ArrayTable("one_result.array")
 ct = ete3.ClusterTree("all_species.tre", data)
-data_max = np.max(ct.arraytable.matrix)
-data_min = np.min(ct.arraytable.matrix)
+data_max = np.max(ct.arraytable.matrix, axis=0)
+data_min = np.min(ct.arraytable.matrix, axis=0)
+data_median = np.median(ct.arraytable.matrix, axis=0)
 
 def mylayout(node):
     if node.is_leaf():
-        profile_face = ProfileFace(data_max, data_min, 0.0, 100, 14, "heatmap")
+        if node.name == "Danio_rerio":
+            # import pudb; pudb.set_trace()
+            pass
+        # profile_face = ProfileFace(data_max[1], data_min[1], 0.0, 100, 14, "heatmap")
+        profile_face = ProfileFace(data_max[0], data_min[0], 0.0, 100, 14, "heatmap")
         ete3.treeview.faces.add_face_to_node(profile_face, node, 0, aligned=True)
+
+        # profile_face = ProfileFace(data_max[0], data_min[0], data_median[0], 100, 14, "heatmap")
+        # ete3.treeview.faces.add_face_to_node(profile_face, node, 1, aligned=True)
 
 ts = TreeStyle()
 ts.show_scale = False
