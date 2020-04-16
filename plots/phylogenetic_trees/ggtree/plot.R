105024390a5c9f8830293a4fb9fc2d806add1591
Felix Stiehler <felix.stiehler87@gmail.com>
2020-04-16 11:44:09 +0200

    moved files around, plant tree data

--- plots/phylogenetic_trees/ggtree/plot.R
+++ plots/phylogenetic_trees/ggtree/plot.R
@@ -4,15 +4,37 @@ library(ggtree)
 library(treeio)
 library(tidytree)
 
-tree = read.nhx('all_species.tre')
-# tree_human = tree_subset(tree, node="Homosapiens", levels_back=5)
+# animals
+tree = read.nhx('animals/all_species.tre')
+
+f1_scores = read.csv('animals/f1_scores.csv', header=TRUE)
+differences = read.csv('animals/differences.csv', header=TRUE)
+sets = read.csv('animals/sets.csv', header=TRUE)
+
+p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set, shape=set, size=.5)) 
+# p = p + geom_label(aes(label=node), fill='steelblue')
+# p = p + geom_cladelabel(node=201, label="Mammalia", align=T, geom='label', offset=12)
+
+png('animals_differences.png', width=1200, height=2000)
+gheatmap(p, differences, width=.1, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5) 
+dev.off()
+
+png('animals_f1_scores.png', width=1200, height=2000)
+gheatmap(p, f1_scores, width=.1, low="red", high="darkgreen", colnames=F, offset=5) 
+dev.off()
 
 
-f1_scores = read.csv('f1_scores.csv', header=TRUE)
-differences = read.csv('differences.csv', header=TRUE)
-sets = read.csv('sets.csv', header=TRUE)
 
-p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set)) 
+# plants
+tree = read.nhx('plants/all_species.tre')
+
+# f1_scores = read.csv('plants/f1_scores.csv', header=TRUE)
+# differences = read.csv('plants/differences.csv', header=TRUE)
+# sets = read.csv('plants/sets.csv', header=TRUE)
+
+# p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set, shape=set, size=.5)) 
+ggtree(tree) # %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set, shape=set, size=.5)) 
+quit()
 # p = p + geom_label(aes(label=node), fill='steelblue')
 # p = p + geom_cladelabel(node=201, label="Mammalia", align=T, geom='label', offset=12)
 
