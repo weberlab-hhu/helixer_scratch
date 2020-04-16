9998dd23ec9e2346766b22c08f58281cc38502ed
Felix Stiehler <felix.stiehler87@gmail.com>
2020-04-16 10:05:47 +0200

    minor fixes

--- plots/phylogenetic_trees/ggtree/plot.R
+++ plots/phylogenetic_trees/ggtree/plot.R
@@ -1,3 +1,4 @@
+# This code need the ggtree fork from https://github.com/soi/ggtree to run
 library(ggplot2)
 library(ggtree)
 library(treeio)
@@ -16,9 +17,9 @@ p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(
 # p = p + geom_cladelabel(node=201, label="Mammalia", align=T, geom='label', offset=12)
 
 png('animals_differences.png', width=1200, height=2000)
-gheatmap(p, differences, width=.2, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5) 
+gheatmap(p, differences, width=.1, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5) 
 dev.off()
 
 png('animals_f1_scores.png', width=1200, height=2000)
-gheatmap(p, f1_scores, width=.2, low="red", high="blue", colnames=F, offset=5) 
+gheatmap(p, f1_scores, width=.1, low="red", high="darkgreen", colnames=F, offset=5) 
 dev.off()
