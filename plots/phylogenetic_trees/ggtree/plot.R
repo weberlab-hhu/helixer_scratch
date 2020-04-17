475c30e53e4c0410a7b89ced831cc4c229427852
Felix Stiehler <felix.stiehler87@gmail.com>
2020-04-17 11:52:34 +0200

    trees

--- plots/phylogenetic_trees/ggtree/plot.R
+++ plots/phylogenetic_trees/ggtree/plot.R
@@ -15,11 +15,11 @@ p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(
 # p = p + geom_label(aes(label=node), fill='steelblue')
 # p = p + geom_cladelabel(node=201, label="Mammalia", align=T, geom='label', offset=12)
 
-png('animals_differences.png', width=1200, height=2000)
+png('animals_differences.png', width=1200, height=3000)
 gheatmap(p, differences, width=.1, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5) 
 dev.off()
 
-png('animals_f1_scores.png', width=1200, height=2000)
+png('animals_f1_scores.png', width=1200, height=3000)
 gheatmap(p, f1_scores, width=.1, low="red", high="darkgreen", colnames=F, offset=5) 
 dev.off()
 
@@ -30,17 +30,16 @@ tree = read.nhx('plants/all_species.tre')
 
 f1_scores = read.csv('plants/f1_scores.csv', header=TRUE)
 differences = read.csv('plants/differences.csv', header=TRUE)
-# sets = read.csv('plants/sets.csv', header=TRUE)
+sets = read.csv('plants/sets.csv', header=TRUE)
 
-# p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set, shape=set, size=.5)) 
-p = ggtree(tree) + geom_tiplab(size=4, offset=.3) # + geom_tippoint(aes(color=set, shape=set, size=.5)) 
+p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set, shape=set, size=.5)) 
 # p = p + geom_label(aes(label=node), fill='steelblue')
 # p = p + geom_cladelabel(node=201, label="Mammalia", align=T, geom='label', offset=12)
 
-png('plants_differences.png', width=1200, height=2000)
-gheatmap(p, differences, width=.1, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5) 
+png('plants_differences.png', width=600, height=1000)
+gheatmap(p, differences, width=.15, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5) 
 dev.off()
 
-png('plants_f1_scores.png', width=1200, height=2000)
-gheatmap(p, f1_scores, width=.1, low="red", high="darkgreen", colnames=F, offset=5) 
+png('plants_f1_scores.png', width=600, height=1000)
+gheatmap(p, f1_scores, width=.15, low="red", high="darkgreen", colnames=F, offset=5) 
 dev.off()
