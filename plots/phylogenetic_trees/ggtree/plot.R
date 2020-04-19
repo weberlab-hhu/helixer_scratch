5f50b2a3d0f27d7c5784f8955f3136ed54bc260f
Felix Stiehler <felix.stiehler87@gmail.com>
2020-04-19 12:29:01 +0200

    tree plots

--- plots/phylogenetic_trees/ggtree/plot.R
+++ plots/phylogenetic_trees/ggtree/plot.R
@@ -11,16 +11,16 @@ f1_scores = read.csv('animals/f1_scores.csv', header=TRUE)
 differences = read.csv('animals/differences.csv', header=TRUE)
 sets = read.csv('animals/sets.csv', header=TRUE)
 
-p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set, shape=set, size=.5)) 
+p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=Set, shape=Set, size=.5)) + guides(size=F) 
 # p = p + geom_label(aes(label=node), fill='steelblue')
 # p = p + geom_cladelabel(node=201, label="Mammalia", align=T, geom='label', offset=12)
 
 png('animals_differences.png', width=1200, height=3000)
-gheatmap(p, differences, width=.1, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5, legend_title="Subgenic F1") 
+gheatmap(p, differences, width=.1, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5, legend_title="Difference") 
 dev.off()
 
 png('animals_f1_scores.png', width=1200, height=3000)
-gheatmap(p, f1_scores, width=.1, colnames=F, offset=5, use_scale_fill_viridis_d=T, legend_title="Subgenic F1") 
+gheatmap(p, f1_scores, width=.1, colnames=F, offset=5, use_scale_fill_viridis_d=T, legend_title="Subgenic F1")
 dev.off()
 
 # plants
@@ -30,12 +30,12 @@ f1_scores = read.csv('plants/f1_scores.csv', header=TRUE)
 differences = read.csv('plants/differences.csv', header=TRUE)
 sets = read.csv('plants/sets.csv', header=TRUE)
 
-p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=set, shape=set, size=.5)) 
+p = ggtree(tree) %<+% sets + geom_tiplab(size=4, offset=.3) + geom_tippoint(aes(color=Set, shape=Set, size=.5)) + guides(size=F) 
 # p = p + geom_label(aes(label=node), fill='steelblue')
 # p = p + geom_cladelabel(node=201, label="Mammalia", align=T, geom='label', offset=12)
 
 png('plants_differences.png', width=600, height=1000)
-gheatmap(p, differences, width=.15, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5, legend_title="Subgenic F1") 
+gheatmap(p, differences, width=.15, low="red", mid="white", high="blue", midpoint=0.0, use_scale_fill_gradient2=T, colnames=F, offset=5, legend_title="Difference") 
 dev.off()
 
 png('plants_f1_scores.png', width=600, height=1000)
