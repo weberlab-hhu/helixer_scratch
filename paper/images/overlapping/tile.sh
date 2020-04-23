d0b063965e5c5ea96ce1061672aa849ba8892506
Felix Stiehler <felix.stiehler87@gmail.com>
2020-04-23 17:01:25 +0200

    position wise eval pics

--- paper/images/overlapping/tile.sh
+++ paper/images/overlapping/tile.sh
@@ -14,7 +14,7 @@ cd $folder
 for offset in $(seq 1 $plots_per_page $total_count)
 do
 	files=$(cat ~/git/helixer_scratch/data_insight/$dataset/genomes_by_n75 | tail +$((offset+1)) | head -$plots_per_page | cut -d "," -f1 | sed -e 's/$/_comparison.png/')
-	montage -mode concatenate -tile $tile $files $pwd_folder/"montage_plants"$count".png"
+	montage -mode concatenate -tile $tile $files $pwd_folder/"montage_"$dataset$count".png"
 	((count++))
 done
 
