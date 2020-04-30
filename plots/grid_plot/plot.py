eb6589fd685ad8bb6f19d2c63564e4283f27fd6c
Felix Stiehler <felix.stiehler87@gmail.com>
2020-04-30 14:40:21 +0200

    plotting

--- plots/grid_plot/plot.py
+++ plots/grid_plot/plot.py
@@ -20,7 +20,7 @@ df_aug = df_aug.drop(columns=['acc_overall', 'f1_ig', 'f1_utr', 'f1_exon', 'f1_i
 
 # add nn values and metadata to aug df
 df_aug['NN'] = df_nn['sub_genic']
-df_aug['total_len'] = df_meta['total_len']
+df_aug['total_len'] = df_meta['total_len'] / 1e9
 df_animals = df_aug.rename(columns={'sub_genic': 'AUG', 'genome': 'species'})
 
 # remove training genomes
@@ -42,7 +42,7 @@ df_meta = df_meta[df_meta['species'].isin(genomes)].sort_values('species').reset
 df_aug = df_aug.drop(columns=['acc_overall', 'f1_ig', 'f1_utr', 'f1_exon', 'f1_intron', 'legacy_f1_cds', 'f1_genic'])
 
 df_aug['NN'] = df_nn['sub_genic']
-df_aug['total_len'] = df_meta['total_len']
+df_aug['total_len'] = df_meta['total_len'] / 1e9
 df_plants = df_aug.rename(columns={'sub_genic': 'AUG', 'genome': 'species'})
 df_plants = df_plants[~df_plants['species'].isin(['Athaliana','Bdistachyon','Creinhardtii','Gmax','Mguttatus','Mpolymorpha',
                                                   'Ptrichocarpa','Sitalica','Zmays'])].reset_index(drop=True)
@@ -51,13 +51,16 @@ df_plants = df_plants[~df_plants['species'].isin(['Athaliana','Bdistachyon','Cre
 df_ol_animals = pd.read_csv('animals_overlapping_aggregate.csv')
 df_ol_plants = pd.read_csv('plants_overlapping_aggregate.csv')
 
+# colors
+aug_color = 'chocolate'
+hel_color = 'royalblue'
+
 # make subplot grid
 fig = plt.figure(figsize=(12, 8))
 outer = gridspec.GridSpec(1, 3, wspace=0.1, hspace=0.1)
 
 # boxplots
-boxplot_grid = gridspec.GridSpecFromSubplotSpec(2, 2,
-                subplot_spec=outer[0], wspace=0.1, hspace=0.1)
+boxplot_grid = gridspec.GridSpecFromSubplotSpec(2, 2, subplot_spec=outer[0], wspace=0.1, hspace=0.1)
 
 # ax = plt.Subplot(fig, boxplot_grid[0, 0])
 # ax = sns.boxplot(y=df_animals["AUG"], medianprops={'color':'red'},
@@ -66,23 +69,43 @@ boxplot_grid = gridspec.GridSpecFromSubplotSpec(2, 2,
     # r, g, b, a = patch.get_facecolor()
     # patch.set_facecolor((r, g, b, 0.0))
 
-def add_subplot(subplot_spec, data, swarm_color, title=''):
+def add_boxplot(subplot_spec, data, swarm_color, title='', tick_labels=False):
     ax = plt.Subplot(fig, subplot_spec)
     ax = sns.boxplot(y=data, medianprops={'color':'red'}, ax=ax, color='white',
                      showfliers=False, showcaps=False, whiskerprops={'linewidth':0})
     plt.setp(ax.lines, zorder=100)
-    ax = sns.swarmplot(y=data, ax=ax, color=swarm_color)
+    ax = sns.swarmplot(y=data, ax=ax, color=swarm_color, size=4)
     ax.set_ylim((0.0, 1.0))
     ax.set_ylabel(None)
     ax.set_xticks([], [])
+    if not tick_labels:
+        ax.set_yticklabels([])
     if title:
         ax.set_title(title)
     fig.add_subplot(ax)
 
-add_subplot(boxplot_grid[0, 0], df_animals['AUG'], 'chocolate', 'AUGUSTUS')
-add_subplot(boxplot_grid[0, 1], df_animals['NN'], 'royalblue', 'Helixer')
-add_subplot(boxplot_grid[1, 0], df_plants['AUG'], 'chocolate')
-add_subplot(boxplot_grid[1, 1], df_plants['NN'], 'royalblue')
+add_boxplot(boxplot_grid[0, 0], df_animals['AUG'], aug_color, tick_labels=True)
+add_boxplot(boxplot_grid[0, 1], df_animals['NN'], hel_color)
+add_boxplot(boxplot_grid[1, 0], df_plants['AUG'], aug_color, tick_labels=True)
+add_boxplot(boxplot_grid[1, 1], df_plants['NN'], hel_color)
+
+# scatterplots
+scatterplot_grid = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=outer[1], wspace=0.1, hspace=0.1)
+
+def add_scatterplot(subplot_spec, data, xlabel=''):
+    ax = plt.Subplot(fig, subplot_spec)
+    ax = sns.regplot(x=data['total_len'], y=data['AUG'], ax=ax, color=aug_color, scatter_kws={'s':8}, line_kws={'linewidth':1.5})
+    ax = sns.regplot(x=data['total_len'], y=data['NN'], ax=ax, color=hel_color, scatter_kws={'s':8}, line_kws={'linewidth':1.5})
+    ax.set_ylim((0.0, 1.0))
+    ax.set_xlim((-0.2, 5.0))
+    ax.set_yticklabels([])
+    ax.set_ylabel(None)
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    fig.add_subplot(ax)
+
+add_scatterplot(scatterplot_grid[0], df_animals)
+add_scatterplot(scatterplot_grid[1], df_plants, 'Genome Size in Gbp')
 
 plt.show()
 
