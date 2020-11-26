7420da6503b2e23446410264c8d56cf4af96a012
Felix Stiehler <felix.stiehler87@gmail.com>
2020-11-26 16:04:39 +0100

    backup scripts

--- backup_scripts/backup-home.sh
+++ backup_scripts/backup-home.sh
@@ -4,12 +4,4 @@ unalias rsync &> /dev/null
 echo
 date
 
-if [ $(ifconfig -a | grep -c tun0) == 1 ]
-then
-	rsync -rzt --stats -e 'ssh -A -o "ProxyJump felix@134.99.224.62"' /home/felix/nni felix@134.99.224.58:/mnt/data/experiments_backup/nni_home_felix/
-	rsync -rzt --stats -e 'ssh -A -o "ProxyJump felix@134.99.224.62"' /home/felix/.local/nnictl felix@134.99.224.58:/mnt/data/experiments_backup/nni_home_felix/
-else
-	echo "Not connected to the vpn, writing data to the cluster"
-	rsync -rzt --stats --exclude='*model*.h5' /home/felix/nni festi100@hpc.rz.uni-duesseldorf.de:/gpfs/project/festi100/backup_home/
-	rsync -rzt --stats /home/felix/.local/nnictl festi100@hpc.rz.uni-duesseldorf.de:/gpfs/project/festi100/backup_home/
-fi
+rsync -rzt --stats -e 'ssh -A -o "ProxyJump felix@134.99.224.59"' /home/felix/nni-experiments felix@134.99.224.58:/mnt/data/experiments_backup/nni_home_felix/
