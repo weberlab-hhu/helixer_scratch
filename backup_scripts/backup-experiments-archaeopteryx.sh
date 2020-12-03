3c5542320bca998221895a7b25254e776a776c3b
alisandra <alisandra.denton@hhu.de>
2020-12-03 19:24:10 +0100

    previously missed

--- backup_scripts/backup-experiments-archaeopteryx.sh
+++ backup_scripts/backup-experiments-archaeopteryx.sh
@@ -9,9 +9,9 @@ echo -e "\nnni denbi no longer active"
 #rsync -rzt --stats --exclude="predictions.h5" triceratops:nni nni_denbi/
 #rsync -rzt --stats --exclude="predictions.h5" triceratops:.local/nnictl nni_denbi/
 
-echo -e "\nnni mordred"
-rsync -rzt --stats  --exclude="predictions.h5" mordred:nni nni_mordred/
-rsync -rzt --stats  --exclude="predictions.h5" mordred:.local/nnictl nni_mordred/
+echo -e "\nnni mordred not actively using"
+#rsync -rLzt --stats  --exclude="predictions.h5" mordred:nni nni_mordred/
+#rsync -rzt --stats  --exclude="predictions.h5" mordred:.local/nnictl nni_mordred/
 
 echo -e "\nnni aardonyx"
 rsync -rzt --stats  --exclude="predictions.h5" aardonyx:nni nni_aliclc/
