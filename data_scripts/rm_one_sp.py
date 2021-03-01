b2a3e2d5a6686c373a8d61bb0018fdd176209f74
Felix Stiehler <felix.stiehler87@gmail.com>
2021-03-01 13:14:01 +0100

    scripts

--- data_scripts/rm_one_sp.py
+++ data_scripts/rm_one_sp.py
@@ -1,14 +1,28 @@
+#! /usr/bin/env python3
 import h5py
+import argparse
 import numpy as np
 
-f = h5py.File('training_data.h5', mode='r')
-newf = h5py.File('training_with_coverage.h5', mode='a')
+parser = argparse.ArgumentParser()
+parser.add_argument('--input-file', type=str, default='', required=True)
+parser.add_argument('--output-file', type=str, default='', required=True)
+parser.add_argument('--remove-genome', type=str, nargs='+', required=True)
+parser.add_argument('--append', action='store_true')
+args = parser.parse_args()
+print(vars(args))
 
-# we want to remove Mguttatus bc we have no stranded RNAseq data for it
-mask = f['data']['species'][:] != b'Mguttatus'
+f = h5py.File(args.input_file, mode='r')
+mode = 'a' if args.append else 'w'
+newf = h5py.File(args.output_file, mode=mode)
 
-#for grp in ['evaluation', 'scores']:
-for grp in ['scores', 'data', 'evaluation']:    
+# build mask for species we want to remove
+mask = np.full_like(True, f['data/species'])
+for genome in args.remove_genomes:
+    mask = np.logical_and(mask, f['data/species'][:] != genome.encode())
+
+print(f'removing {np.sum(mask)) / len(mask) * 100:.2f}% of samples'
+
+for grp in f.keys():
     newf.create_group(grp)
     # setup datasets
     odat = f[grp]
@@ -22,27 +36,29 @@ for grp in ['scores', 'data', 'evaluation']:
                             dtype=odat[key].dtype,
                             compression='lzf',
                             shuffle=odat[key].shuffle)
-    by = 500
+    by = 5000
     for i in range(0, f['data/y'].shape[0], by):
         submask = mask[i:(i + by)]
         n_seqs = np.sum(submask)
-        old_end = ndat[list(ndat.keys())[0]].shape[0]
+        if n_seqs > 0:
+            old_end = ndat[list(ndat.keys())[0]].shape[0]
 
-        for dset_key in odat.keys():
-            # expand as necessary
-            dset = ndat[dset_key]
-            dset.resize(old_end + n_seqs, axis=0)
+            for dset_key in odat.keys():
+                # expand as necessary
+                dset = ndat[dset_key]
+                dset.resize(old_end + n_seqs, axis=0)
 
-            # copy over
-            dset[old_end:] = odat[dset_key][i:(i + by)][submask]
-    newf.flush()
+                # copy over
+                dset[old_end:] = odat[dset_key][i:(i + by)][submask]
+            newf.flush()
 
 # copy over the metadata, start_end_i will be wrong, everything else should be OK
-newf.create_group('meta')
-for key in f['meta'].keys():
+if 'meta' in newf.keys():
+    newf.create_group('meta')
+    for key in f['meta'].keys():
 
-    bkey = key.encode('utf-8')
-    h5py.h5o.copy(f['meta'].id, bkey, newf['meta'].id, bkey)
+        bkey = key.encode('utf-8')
+        h5py.h5o.copy(f['meta'].id, bkey, newf['meta'].id, bkey)
 
 newf.close()
 f.close()
--- data_scripts/sample-single-genomes.py
+++ data_scripts/sample-single-genomes.py
@@ -14,13 +14,14 @@ parser.add_argument('--main-folder', type=str, default='', required=True,
 parser.add_argument('--output-file', type=str, default='./generalization_validation.h5')
 parser.add_argument('--coefficient', type=float, default=30.0, help='Can not be too large.')
 parser.add_argument('--exponent', type=float, default=0.45)
-parser.add_argument('--max-samples', type=int, default=0, help='Maximum samples taken from one genome if > 0',
-                    help='Maximum samples taken from one genome if > 0')
+parser.add_argument('--max-samples', type=int, default=0, help='Maximum samples taken from one genome if > 0')
 parser.add_argument('--skip-datasets', type=str, nargs='+', default=['gene_lengths', 'err_samples', 'fully_intergenic_samples'])
+parser.add_argument('--dry-run', action='store_true', help='Just output what would be done')
 args = parser.parse_args()
 print(vars(args))
 
-h5_out = h5py.File(args.output_file, 'w')
+if not args.dry_run:
+    h5_out = h5py.File(args.output_file, 'w')
 for i, folder in enumerate(os.listdir(args.main_folder)):
     start_time = time.time()
     h5_in = h5py.File(os.path.join(args.main_folder, folder, 'test_data.h5'), 'r')
@@ -34,22 +35,22 @@ for i, folder in enumerate(os.listdir(args.main_folder)):
         n_samples = min(args.max_samples, n_samples)
 
     samples_idx = sorted(np.random.choice(n_samples_source, n_samples, replace=False))
-    print(f'selecting {n_samples} samples of {folder}')
-    for key in h5_in['data'].keys():
-        if key not in args.skip_datasets:
-            samples = dsets_in[key][samples_idx]
-            if i == 0:
-                HelixerExportController._create_dataset(h5_out, f'/data/{key}', samples, dsets_in[key].dtype)
-                dsets_out = h5_out['data']
-            old_len = dsets_out[key].shape[0]
-            dsets_out[key].resize(old_len + n_samples, axis=0)
-            dsets_out[key][old_len:] = samples
+    print(f'selecting {n_samples} samples of {folder}', flush=True)
+    if not args.dry_run:
+        for key in h5_in['data'].keys():
+            if key not in args.skip_datasets:
+                samples = dsets_in[key][samples_idx]
+                if i == 0:
+                    HelixerExportController._create_dataset(h5_out, f'/data/{key}', samples, dsets_in[key].dtype)
+                    dsets_out = h5_out['data']
+                old_len = dsets_out[key].shape[0]
+                dsets_out[key].resize(old_len + n_samples, axis=0)
+                dsets_out[key][old_len:] = samples
     h5_in.close()
-    print(f'added {n_samples} / {n_samples_source} samples of {folder} in {time.time() - start_time:.2f} secs')
+    print(f'added {n_samples} / {n_samples_source} samples of {folder} in {time.time() - start_time:.2f} secs', flush=True)
 
-h5_out.attrs['timestamp'] = datetime.datetime.now()
-h5_out.attrs['coefficient'] = args.coefficient
-h5_out.attrs['exponent'] = args.exponent
-h5_out.attrs['max_samples'] = args.max_samples
-h5_out.attrs['skip_datasets'] = args.skip_datasets
-h5_out.close()
+if not args.dry_run:
+    h5_out.attrs['timestamp'] = datetime.datetime.now()
+    for key, value in args.items():
+        h5_out.attrs[key] = value
+    h5_out.close()
