807a2d9e64b2f3cdcbe5e418e2b7166ba18f0234
Felix Stiehler <felix.stiehler87@gmail.com>
2021-03-17 09:58:49 +0100

    variable length sampling in parallel, highly effective

--- data_scripts/sample-single-genomes-variable-length.py
+++ data_scripts/sample-single-genomes-variable-length.py
@@ -6,29 +6,11 @@ import h5py
 import argparse
 import numpy as np
 import datetime
+from multiprocess import Pool
 from helixer.export.exporter import HelixerExportController
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--main-folder', type=str, default='', required=True,
-                    help=('Main single genome folder. Expects "test_data.h5" files '
-                          'inside "single_genomes_{20,50,100,200}k" subfolders.'))
-parser.add_argument('--output-folder', type=str, default='', required=True)
-parser.add_argument('--genome-length-file', type=str, default='animals_with_input_length',
-                    help='CSV file that links genomes to their input length')
-parser.add_argument('--max-samples', type=int, default=20000,
-                    help='Maximum samples taken from one genome. Accounts for what would be taken at length 20k.')
-parser.add_argument('--skip-datasets', type=str, nargs='+', default=['gene_lengths'])
-args = parser.parse_args()
-print(vars(args))
-
-os.makedirs(args.output_folder, exist_ok=True)
-
-n_total_samples = 0
-for line in open(args.genome_length_file, 'r'):
+def sample_file(species, input_len):
     start_time = time.time()
-    species = line.split(',')[0]
-    input_len = int(line.split(',')[1])
-
     h5_in = h5py.File(os.path.join(args.main_folder, f'single_genomes_{input_len}k', species, 'test_data.h5'), 'r')
     h5_out = h5py.File(os.path.join(args.output_folder, f'{species}.h5'), 'w')
     dsets_in = h5_in['data']
@@ -37,7 +19,6 @@ for line in open(args.genome_length_file, 'r'):
     n_samples_source = dsets_in['X'].shape[0]
     max_samples_adjusted = int(args.max_samples / (input_len / 20))  # take less sequences if input_len is longer
     n_samples = min(max_samples_adjusted, n_samples_source)
-    n_total_samples += n_samples
 
     samples_idx = sorted(np.random.choice(n_samples_source, n_samples, replace=False))
     print(f'selecting {n_samples} samples of {species} with length {input_len}', flush=True)
@@ -48,11 +29,36 @@ for line in open(args.genome_length_file, 'r'):
             dsets_out[key].resize(n_samples, axis=0)
             dsets_out[key][:] = samples
     h5_in.close()
+
+    h5_out.attrs['timestamp'] = str(datetime.datetime.now())
+    for key, value in vars(args).items():
+        h5_out.attrs[key] = value
+    h5_out.close()
     print(f'added {n_samples} / {n_samples_source} samples of {species} in {time.time() - start_time:.2f} secs', flush=True)
 
-h5_out.attrs['timestamp'] = str(datetime.datetime.now())
-for key, value in vars(args).items():
-    h5_out.attrs[key] = value
-h5_out.close()
+parser = argparse.ArgumentParser()
+parser.add_argument('--main-folder', type=str, default='', required=True,
+                    help=('Main single genome folder. Expects "test_data.h5" files '
+                          'inside "single_genomes_{20,50,100,200}k" subfolders.'))
+parser.add_argument('--output-folder', type=str, default='', required=True)
+parser.add_argument('--genome-length-file', type=str, default='animals_with_input_length',
+                    help='CSV file that links genomes to their input length')
+parser.add_argument('--max-samples', type=int, default=20000,
+                    help='Maximum samples taken from one genome. Accounts for what would be taken at length 20k.')
+parser.add_argument('--n-processes', type=int, default=8, help='May be useful due to compression even on single HDD machines.')
+parser.add_argument('--skip-datasets', type=str, nargs='+', default=['gene_lengths'])
+args = parser.parse_args()
+print(vars(args))
 
-print(f'done, added {n_total_samples} in total')
+os.makedirs(args.output_folder, exist_ok=True)
+
+n_total_samples = 0
+species, input_lens = [], []
+for line in open(args.genome_length_file, 'r'):
+    species.append(line.split(',')[0])
+    input_lens.append(int(line.split(',')[1]))
+
+with Pool(args.n_processes) as p:
+    n_samples = p.starmap(sample_file, zip(species, input_lens))
+
+print(f'done')
