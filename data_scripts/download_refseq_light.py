3ce83a6dd01b93472c7855dc37b22751e5f9a00d
alisandra <alisandra.denton@hhu.de>
2022-03-02 20:10:27 +0100

    getting out the soup

--- data_scripts/download_refseq_light.py
+++ data_scripts/download_refseq_light.py
@@ -1,7 +1,47 @@
 import click
 import subprocess
 import os
+import shutil
 from bs4 import BeautifulSoup
+
+
+ospj = os.path.join
+
+class DirHolder:
+    def __init__(self, out_dir):
+        self.out_dir = out_dir
+        # clean previous
+        if os.path.exists(self.working):
+            shutil.rmtree(self.working)
+        # setup
+        for d in [self.working, self.latest, self.version]:
+            os.path.makedirs(d)
+
+    @property
+    def working(self):
+        return ospj(self.out_dir, 'working')
+
+    @property
+    def latest(self):
+        return ospj(self.out_dir, 'latest')
+
+    @property
+    def version(self):
+        return ospj(self.out_dir, 'version')
+    
+
+    def get_version_from_latest(self):
+        with open(ospj(self.latest, 'index.html')) as f:
+            soup = BeautifulSoup(f.read(), 'html.parser')
+        links = soup.find_all('a')
+        assert len(links) == 1, f'len(links) != 1, instead {len(links)}'
+        return links[0]["href"] + '/'
+
+    def get_genome_fa_from_version(self):
+        pass
+
+    def get_genome_gff_from_version(self):
+        pass
 
 
 @click.command()
@@ -9,14 +49,12 @@ def cli():
     ftp_pfx = 'ftp://'
     basepath = "ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_other/"
     species = "Seriola_lalandi"
-    workingdir = os.path.join(species, 'working')
-    if not os.path.exists(workingdir):
-        os.makedirs(workingdir)
+    dh = DirHolder(species)
     target = f'{ftp_pfx}{basepath}{species}/latest_assembly_versions/'
-    #subprocess.run(['wget', target], cwd=workingdir)
-    with open(os.path.join(workingdir, 'index.html')) as f:
-        soup = BeautifulSoup(f.read(), 'html.parser')
-    print(soup.find_all('a'))
+    subprocess.run(['wget', target], cwd=dh.latest)
+    genome_version = dh.get_version_from_latest()
+    datadir = f'{target}/{genome_version}/'
+    subprocess.run(['wget', target], cwd=dh.version)
 
 
 if __name__ == "__main__":
