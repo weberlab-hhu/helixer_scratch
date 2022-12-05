236b3062e6928e31d8658c51bead6861021b243a
alisandra <alisandra.denton@hhu.de>
2022-12-05 16:08:38 +0100

    pulling out phase too

---
+++ cluster_eval_predictions/evaluations/overall/more-stats-from-eval-one-trial_nothpc.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+if [[ $# -lt 2 ]]; then
+	echo "Usage: ./more-stats-from-eval-one-trial_nothpc.sh trial_folder eval_file_name"
+	exit
+fi
+
+trial_folder=$1
+eval_file_name=$2
+
+# if the folder has a trial.log in it we assume nni, otherwise cluster
+log_file="$trial_folder/$eval_file_name"
+dataset=$(cat $log_file| grep -v load_model_path | grep ": '\(.*_data.h5\)'" -o|sed "s/[: ']//g")
+model=$(cat $log_file| grep "'load_model_path': '\(.*.h5\)'" -o|sed "s/[: ']//g;s/load_model_path//g")
+
+dataset=${dataset%/*_data.h5}
+dataset=`echo $dataset|sed 's@.*/@@g'`
+
+#model=`echo $model|sed 's@.*/@@g'`
+
+
+
+
+genicf1=`cat $trial_folder/$eval_file_name |grep '| genic'|sed 's/ //g'|cut -f5 -d'|'`
+subgenicf1=`cat $trial_folder/$eval_file_name |grep '| sub_genic'|sed 's/ //g'|cut -f5 -d'|'`
+phase_2=`cat $trial_folder/$eval_file_name |grep '| phase_2 '|sed 's/ //g'|cut -f5 -d'|'`
+phase_none=`cat $trial_folder/$eval_file_name |grep '| no_phase '|sed 's/ //g'|cut -f5 -d'|'`
+
+
+echo "$model,$dataset,$genicf1,$subgenicf1,$phase_2,$phase_none"
---
+++ cluster_eval_predictions/evaluations/overall/more-stats-from-eval_nothpc.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# evaluates predictions (what would have been in trial.log files when done with nni)
+# from a run that only generated predictions.h5 files in different folders
+# this is useful to save time as this step can be done concurrently
+# uses eval-from-predictions-one-trial.sh to work concurrently
+
+if [[ $# -lt 2 ]]; then
+	echo "Usage: ./more-stats-from-eval.sh main_folder output_file_name"
+	exit
+fi
+here=`readlink -f $0`
+here=${here%/more-stats-from-eval*sh}
+main_folder=$1
+output_file_name=$2
+
+for i in `ls -d "$main_folder"/*/`;
+do 
+  $here/more-stats-from-eval-one-trial_nothpc.sh $i  $output_file_name
+done
+
